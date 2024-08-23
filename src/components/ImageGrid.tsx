0699f059ab1dcf99c220129d4c0569293333f6a2
Johan Fagerberg <johanringmann@gmail.com>
2024-08-23 17:06:19 +0200

    feat: add yet-another-medium-zoom

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -1,7 +1,27 @@
 import type React from "react";
+import yamz from "yet-another-medium-zoom";
 
 import style from "./ImageGrid.module.css";
+import { useEffect, useRef, useState } from "react";
 
 export default function ImageGrid(props: React.PropsWithChildren<{}>) {
-  return <div className={style.grid}>{props.children}</div>;
+  const $ref = useRef<HTMLDivElement>(null);
+  const [isBound, setIsBound] = useState(false);
+  useEffect(() => {
+    if (!$ref.current) {
+      setIsBound(false);
+      return;
+    }
+    if (isBound) return;
+    const $images = [
+      ...$ref.current.querySelectorAll("img, picture"),
+    ] as HTMLElement[];
+    yamz.bind($images);
+    setIsBound(false);
+  }, [$ref.current]);
+  return (
+    <div ref={$ref} className={style.grid}>
+      {props.children}
+    </div>
+  );
 }
