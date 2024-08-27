2bc7e9920ffc6948d19cb78e0d139ebb2aa0b72d
Johan Fagerberg <johanringmann@gmail.com>
2024-08-27 14:44:36 +0200

    feat: add hero image

--- src/layouts/IndexLayout.tsx
+++ src/layouts/IndexLayout.tsx
@@ -1,28 +1,9 @@
 import type { PropsWithChildren } from "react";
 import RootLayout from "./RootLayout";
 
-import style from "./IndexLayout.module.css";
+export type IndexLayoutProps = {};
 
-export type IndexLayoutProps = {
-  title: string;
-};
-
-const IndexLayout = ({
-  children,
-  title,
-}: PropsWithChildren<IndexLayoutProps>) => {
-  return (
-    <RootLayout>
-      <h1 className={style.hero}>{title}</h1>
-      <nav>
-        <a href="#velkommen">Velkommen</a>
-        <a href="#faciliteter">Faciliteter</a>
-        <a href="#regler">Regler</a>
-        <a href="#priser">Priser</a>
-        <a href="#om-os">Om os</a>
-      </nav>
-      {children}
-    </RootLayout>
-  );
+const IndexLayout = ({ children }: PropsWithChildren<IndexLayoutProps>) => {
+  return <RootLayout>{children}</RootLayout>;
 };
 export default IndexLayout;
